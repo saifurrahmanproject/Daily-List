57dac19eeec5ebe96ac9c2e4dcc6135bb5def750
saifurrahmanproject <s.r.tonoy.p@gmail.com>
2020-12-01 00:41:23 -0500

    Initial Commit

---
+++ Daily List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily List
+//
+//  Created by Tonoy Rahman on 2020-12-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
