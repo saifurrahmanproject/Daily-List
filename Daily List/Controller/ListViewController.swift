b7eb8c1c5aeb88896d9604168a681f8d25da65ec
saifurrahmanproject <s.r.tonoy.p@gmail.com>
2020-12-07 23:51:49 -0500

    add functions to save data.

--- Daily List/Controller/ListViewController.swift
+++ Daily List/Controller/ListViewController.swift
@@ -20,28 +20,7 @@ class ListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-   
-        
-        let newItem = Item()
-        newItem.title = "Buy Mike"
-        itemArray.append(newItem)
-
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggs"
-        itemArray.append(newItem2)
-
-        let newItem3 = Item()
-        newItem3.title = "Buy Rice"
-        itemArray.append(newItem3)
-        
-        
-        
-
-//        if let items = defaults.array(forKey: "DailyListArray") as? [Item] {
-//
-//            itemArray = items
-//        }
-        
+        loadItems()
     }
     
     
@@ -139,5 +118,18 @@ class ListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            
+            let decoder = PropertyListDecoder()
+            
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
+    }
     
 }
